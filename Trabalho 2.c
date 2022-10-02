c61665200f6f994c9ff904c3e11c3b1b9059c462
EnthonyBohm <enthonygrbohm@gmail.com>
2022-10-02 11:56:30 -0300

    programa em switch case e resposta final aceita minuscula

--- Trabalho 2.c
+++ Trabalho 2.c
@@ -28,7 +28,7 @@ int main(){
 	}
 //	printf("%d",esc_pc);
 	
-	printf("Vamos jogar um jogo?\nPedra Papel Tesoura Lagarto e Spock\nVamos escolha um \n1.Pedra \n2.Papel \n3.Tesoura\n4. Lagarto \n5. Spock \n");
+	printf("\n\nVamos jogar um jogo?\nPedra Papel Tesoura Lagarto e Spock\nVamos escolha um \n1.Pedra \n2.Papel \n3.Tesoura\n4. Lagarto \n5. Spock \n");
 	scanf("%d", &esc_us);
 	while(esc_us <1 || esc_us >5){
 		printf("Valor inválido, digite novamente: ");
@@ -36,89 +36,125 @@ int main(){
 	}
 	printf("\n------------------------------------------\n");
 	
-	if(esc_us == 1){
-		if (esc_pc == 1){
+	switch (esc_us){
+	
+	case 1:
+		switch (esc_pc){
+			case 1:
 			printf("Pedra empata com pedra");
-		}
-		if (esc_pc == 2){
+			break;
+			
+			case 2:
 			printf("Papel Amassa pedra \nVocê Perdeu");
-		}
-		if (esc_pc == 3){
+			break;
+			
+			case 3:
 			printf("Pedra Quebra Tesoura \nVocê Ganhou, Parabéns!");
-		}
-		if (esc_pc == 4){
+			break;
+			
+			case 4:
 			printf("Pedra esmaga lagarto \nVocê Ganhou, Parabéns!");
-		}
-		if (esc_pc == 5){
+			break;
+			
+			case 5:
 			printf("Spock vaporiza pedra \nVocê perdeu");
+			break;
 		}
-	}
-	if(esc_us == 2){
-		if (esc_pc == 1){
+	break;
+
+	case 2:
+		switch (esc_pc){
+			case 1:
 			printf("Papel Amassa pedra \nVocê ganhou, Parabéns! ");
-		}
-		else if(esc_pc == 2){
+			break;
+			
+			case 2:
 			printf("Papel empata com papel");
-		}
-		else if (esc_pc == 3){
+			break;
+			
+			case 3:
 			printf("Tesoura corta Papel \nVocê perdeu");
-		}
-		else if(esc_pc == 4){
+			break;
+			
+			case 4:
 			printf("Lagarto come papel \nVocê perdeu");
-		}
-		else if(esc_pc == 5){
+			break;
+			
+			case 5:
 			printf("Papel refuta Spock \nVocê ganhou,Parabéns!");
+			break;
 		}
-	}
-	if (esc_us == 3){
-		if (esc_pc == 1){
+	break;
+	
+	case 3:
+		switch (esc_pc){
+			case 1:
 			printf("Pedra Quebra Tesoura \nVocê Perdeu");
-		}
-		if (esc_pc == 2){
+			break;
+			
+			case 2:
 			printf("Tesoura corta o Papel \nVocê ganhou, Parabéns!");
-		}
-		if (esc_pc == 3){
+			break;
+			
+			case 3:
 			printf("Tesoura empata com tesoura");
-		}
-		if (esc_pc == 4){
+			break;
+			
+			case 4:
 			printf("Tesoura decapita lagarto \nVocê ganhou,Parabéns!");
-		}
-		if (esc_pc == 5){
+			break;
+			
+			case 5:
 			printf("Spock derrete tesoura \nVocê perdeu");
+			break;
 		}
-	}
-	if (esc_us == 4){
-		if(esc_pc == 1){
+	break;
+	
+	case 4:
+		switch (esc_pc){
+			case 1:
 			printf("Pedra esmaga lagarto \nVocê perdeu");
-		}
-		if(esc_pc == 2){
+			break;
+			
+			case 2:
 			printf("Lagarto come papel \nVocê ganhou,Parabéns!");
-		}
-		if(esc_pc == 3){
+			break;
+			
+			case 3:
 			printf("Tesoura decapita Lagarto \nVocê perdeu");
-		}
-		if(esc_pc == 4){
+			break;
+			
+			case 4:
 			printf("Lagarto empata com lagarto ");
-		}
-		if(esc_pc == 5){
+			break;
+			
+			case 5:
 			printf("Lagarto envenena Spock \nVocê ganhou, Parabéns!");
+			break;
 		}
-	}
-	if (esc_us == 5){
-		if(esc_pc == 1){
+	break;
+	
+	case 5:
+		switch (esc_pc){
+			case 1:
 			printf("Spock vaporiza pedra \nVocê ganhou, Parabéns!");
-		}
-		if(esc_pc == 2){
+			break;
+			
+			case 2:
 			printf("Papel refuta Spock \nVocê perdeu");
-		}
-		if(esc_pc == 3){
+			break;
+			
+			case 3:
 			printf("Spock derrete tesoura \nVocê ganhou, Parabéns!");
-		}
-		if(esc_pc == 4){
+			break;
+			
+			case 4:
 			printf("Lagarto envenena Spock \nVocê perdeu");
-		}
-		if(esc_pc == 5){
+			break;
+			
+			case 5:
 			printf("Spock empata com Spock");
+			break;
 		}
 	}
 		
@@ -126,10 +162,10 @@ int main(){
 		printf("\nVocê deseja jogar novamente? (S/N)");
 		scanf("    %c",&d);
 		
-		while(d != 'S' && d != 'N'){
-			printf("Resposta inválida, digite novamente (USE MAÍUSCULA!)");
+		while(d != 'S' && d != 'N' && d != 's' && d!= 'n'){
+			printf("Resposta inválida, digite novamente ");
 			scanf("  %c",&d);
 		}
-    }while(d == 'S');
+    }while(d == 'S' || d== 's');
     return 0;
 }
