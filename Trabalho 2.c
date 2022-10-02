89fe9415d4d3886bf7ba8f4846a8b9d419d7f2eb
EnthonyBohm <enthonygrbohm@gmail.com>
2022-10-02 11:04:09 -0300

    lagarto Spock added

--- Trabalho 2.c
+++ Trabalho 2.c
@@ -1,3 +1,17 @@
+/*
+*Tesoura corta papel
+*Papel cobre pedra
+*Pedra esmaga lagarto
+*Lagarto envenena Spock
+*Spock derrete tesoura
+*Tesoura decapita Lagarto
+*Lagarto come papel
+*Papel refuta Spock
+*Spock vaporiza pedra
+*Pedra amassa tesoura
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,15 +22,15 @@ int main(){
 	do{
 	
 	srand(time(NULL));
-	esc_pc= rand()%3;
+	esc_pc= rand()%6;
 	while(esc_pc == 0){
-		esc_pc= rand()%4;
+		esc_pc= rand()%6;
 	}
 //	printf("%d",esc_pc);
 	
-	printf("Vamos jogar um jogo?\nPedra Papel Tesoura\nVamos escolha um \n1.Pedra \n2.Papel \n3.Tesoura\n");
+	printf("Vamos jogar um jogo?\nPedra Papel Tesoura Lagarto e Spock\nVamos escolha um \n1.Pedra \n2.Papel \n3.Tesoura\n4. Lagarto \n5. Spock \n");
 	scanf("%d", &esc_us);
-	while(esc_us <1 || esc_us >3){
+	while(esc_us <1 || esc_us >5){
 		printf("Valor inválido, digite novamente: ");
 		scanf("%d", &esc_us);
 	}
@@ -26,14 +40,20 @@ int main(){
 		if (esc_pc == 1){
 			printf("Pedra empata com pedra");
 		}
-		else if (esc_pc == 2){
+		if (esc_pc == 2){
 			printf("Papel Amassa pedra \nVocê Perdeu");
 		}
-		else if (esc_pc == 3){
+		if (esc_pc == 3){
 			printf("Pedra Quebra Tesoura \nVocê Ganhou, Parabéns!");
 		}
+		if (esc_pc == 4){
+			printf("Pedra esmaga lagarto \nVocê Ganhou, Parabéns!");
+		}
+		if (esc_pc == 5){
+			printf("Spock vaporiza pedra \nVocê perdeu");
+		}
 	}
-	else if(esc_us == 2){
+	if(esc_us == 2){
 		if (esc_pc == 1){
 			printf("Papel Amassa pedra \nVocê ganhou, Parabéns! ");
 		}
@@ -43,18 +63,65 @@ int main(){
 		else if (esc_pc == 3){
 			printf("Tesoura corta Papel \nVocê perdeu");
 		}
+		else if(esc_pc == 4){
+			printf("Lagarto come papel \nVocê perdeu");
+		}
+		else if(esc_pc == 5){
+			printf("Papel refuta Spock \nVocê ganhou,Parabéns!");
+		}
 	}
-	else if (esc_us == 3){
+	if (esc_us == 3){
 		if (esc_pc == 1){
 			printf("Pedra Quebra Tesoura \nVocê Perdeu");
 		}
-		else if (esc_pc == 2){
+		if (esc_pc == 2){
 			printf("Tesoura corta o Papel \nVocê ganhou, Parabéns!");
 		}
-		else if (esc_pc == 3){
+		if (esc_pc == 3){
 			printf("Tesoura empata com tesoura");
 		}
+		if (esc_pc == 4){
+			printf("Tesoura decapita lagarto \nVocê ganhou,Parabéns!");
+		}
+		if (esc_pc == 5){
+			printf("Spock derrete tesoura \nVocê perdeu");
+		}
 	}
+	if (esc_us == 4){
+		if(esc_pc == 1){
+			printf("Pedra esmaga lagarto \nVocê perdeu");
+		}
+		if(esc_pc == 2){
+			printf("Lagarto come papel \nVocê ganhou,Parabéns!");
+		}
+		if(esc_pc == 3){
+			printf("Tesoura decapita Lagarto \nVocê perdeu");
+		}
+		if(esc_pc == 4){
+			printf("Lagarto empata com lagarto ");
+		}
+		if(esc_pc == 5){
+			printf("Lagarto envenena Spock \nVocê ganhou, Parabéns!");
+		}
+	}
+	if (esc_us == 5){
+		if(esc_pc == 1){
+			printf("Spock vaporiza pedra \nVocê ganhou, Parabéns!");
+		}
+		if(esc_pc == 2){
+			printf("Papel refuta Spock \nVocê perdeu");
+		}
+		if(esc_pc == 3){
+			printf("Spock derrete tesoura \nVocê ganhou, Parabéns!");
+		}
+		if(esc_pc == 4){
+			printf("Lagarto envenena Spock \nVocê perdeu");
+		}
+		if(esc_pc == 5){
+			printf("Spock empata com Spock");
+		}
+	}
+		
 		printf("\n------------------------------------------\n");
 		printf("\nVocê deseja jogar novamente? (S/N)");
 		scanf("    %c",&d);
